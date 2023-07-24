0e70c1cb8c12efa58ded05adae16c36c4b929e2b
Daniel Ayala <danielayala1793@hotmail.com>
2023-07-23 19:44:36 -0500

    Primera subida del proyecto 23/7/2023

---
+++ html/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Trotamundos</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/index.css?ts=<?= time() ?>">
+        <script defer src="../font_awesome/js/all.js"></script>
+        <link rel="stylesheet" href="bootstrap5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="bootstrap5/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="contenedor">
+            <header>
+                
+            </header>
+            <nav>
+                <div id="search">
+                    <input type="text" placeholder="Buscar"><i class="fas fa-search"></i>
+                </div>
+                <div id="options">
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Sobre mí</li>
+                        <li>Destinos</li>
+                        <li>Gastronomía</li>
+                        <li>Relax</li>
+                        <li>Videos</li>
+                    </ul>
+                </div>
+                <div id="socialIcons">
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div id="portada">
+                <div>
+                    <p>Blog de Viajes</p>
+                    <p>Trotamundos</p>
+                    <p>No he recorrido todo el mundo, pero pienso hacerlo</p>
+                </div>
+                <img src="https://static.wixstatic.com/media/5bfb6f_26f1a5c736e544e09c63c82a4c792645~mv2_d_3839_1306_s_2.jpg/v1/fill/w_1349,h_475,al_b,q_85,usm_0.66_1.00_0.01/5bfb6f_26f1a5c736e544e09c63c82a4c792645~mv2_d_3839_1306_s_2.webp" alt="Image">
+            </div>
+            <section id="shortImages">
+                <div>
+                    <h3>Destinos</h3>
+                    <img src="https://static.wixstatic.com/media/5bfb6f_9f2519d5fc2d41f990a10dd92eb8658d.jpg/v1/fill/w_314,h_260,al_c,q_80,usm_0.66_1.00_0.01/5bfb6f_9f2519d5fc2d41f990a10dd92eb8658d.webp" alt="Image">
+                </div>
+                <div>
+                    <h3>Gastronomía</h3>
+                    <img src="https://static.wixstatic.com/media/5bfb6f_ea37f67cad544b348df9f3d0ff40f282.jpg/v1/fill/w_314,h_260,al_c,q_80,usm_0.66_1.00_0.01/5bfb6f_ea37f67cad544b348df9f3d0ff40f282.webp" alt="Image">
+                </div>
+                <div>
+                    <h3>Relax</h3>
+                    <img src="https://static.wixstatic.com/media/5bfb6f_c667d0c9bd634b1aa2b29311795f3c51.jpg/v1/fill/w_315,h_260,al_c,q_80,usm_0.66_1.00_0.01/5bfb6f_c667d0c9bd634b1aa2b29311795f3c51.webp" alt="Image">
+                </div>
+            </section>
+            <section id="menciones">
+                <div>
+                    Mencionado en
+                </div>
+                <div>
+                    <p>blog</p>
+                    <h3>de viajes</h3>
+                </div>
+                <div>
+                    <i class="fas fa-fighter-jet"></i>
+                    <p>PLANE<b>TOUR</b></p>
+                </div>
+                <div>
+                    <h2>MUNDO</h2>
+                    <h2>VIAJES</h2>
+                </div>
+                <div>
+                    <div>
+                        <i class="fas fa-cloud"></i>
+                    </div>
+                    <h2>skycloud</h2>
+                </div>
+            </section>
+            <section id="notas">
+                <div class="card"><!--caja completa-->
+                    <img src="https://static.wixstatic.com/media/da1004557f254d4b9d4166b1bf32c599.jpg/v1/fill/w_940,h_529,fp_0.50_0.50,q_90/da1004557f254d4b9d4166b1bf32c599.webp" class="card-img-top" alt="...">
+                    <div class="card-body"><!--caja de texto-->
+                        <div><!--caja 1-->
+                            <div><i class="fas fa-user-circle"></i></div>
+                            <p class="card-text">Administrador <br>jul 15 • 1 min.</p>
+                        </div>
+                        <div><!--caja 2-->
+                            <a href="#">
+                                <h2>Playas de arena sedosa</h2>
+                                <p>Crea un subtítulo para la entrada del blog que resuma dicha publicación en un par de oraciones e invite a tu audiencia a continuar...</p><hr>
+                            </a>
+                        </div>
+                        <div><!--caja 3-->
+                            <div><i class="far fa-eye"></i><p>58</p></div>
+                            <div><i class="far fa-comment-alt"></i><p>0</p></div>
+                            <div><p>12</p><i class="far fa-heart"></i></div>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://static.wixstatic.com/media/4d2d8fa0c42543a7b9a5ec9416597378.jpg/v1/fill/w_940,h_529,fp_0.50_0.50,q_90/4d2d8fa0c42543a7b9a5ec9416597378.webp" class="card-img-top" alt="...">
+                    <div class="card-body"><!--caja de texto-->
+                        <div><!--caja 1-->
+                            <div><i class="fas fa-user-circle"></i></div>
+                            <p class="card-text">Administrador <br>jul 15 • 1 min.</p>
+                        </div>
+                        <div><!--caja 2-->
+                            <a href="#">
+                                <h2>Playas de arena sedosa</h2>
+                                <p>Crea un subtítulo para la entrada del blog que resuma dicha publicación en un par de oraciones e invite a tu audiencia a continuar...</p><hr>
+                            </a>
+                        </div>
+                        <div><!--caja 3-->
+                            <div><i class="far fa-eye"></i><p>58</p></div>
+                            <div><i class="far fa-comment-alt"></i><p>0</p></div>
+                            <div><p>12</p><i class="far fa-heart"></i></div>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://static.wixstatic.com/media/38c4af340de54c78b4e1869be78ef1fd.jpg/v1/fill/w_940,h_529,fp_0.50_0.50,q_90/38c4af340de54c78b4e1869be78ef1fd.webp" class="card-img-top" alt="...">
+                    <div class="card-body"><!--caja de texto-->
+                        <div><!--caja 1-->
+                            <div><i class="fas fa-user-circle"></i></div>
+                            <p class="card-text">Administrador <br>jul 15 • 1 min.</p>
+                        </div>
+                        <div><!--caja 2-->
+                            <a href="#">
+                                <h2>Playas de arena sedosa</h2>
+                                <p>Crea un subtítulo para la entrada del blog que resuma dicha publicación en un par de oraciones e invite a tu audiencia a continuar...</p><hr>
+                            </a>
+                        </div>
+                        <div><!--caja 3-->
+                            <div><i class="far fa-eye"></i><p>58</p></div>
+                            <div><i class="far fa-comment-alt"></i><p>0</p></div>
+                            <div><p>12</p><i class="far fa-heart"></i></div>
+                        </div>                        
+                    </div>
+                </div>
+            </section>
+            <div id="socialLinks">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i><h4>Facebook</h4></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i><h4>Instagram</h4></a></li>
+                    <li><a href="#"><i class="fab fa-pinterest-p"></i><h4>Pinterest</h4></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i><h4>Twitter</h4></a></li>
+                </ul>
+            </div>
+            <div id="attractions">
+                <ul>
+                    <li><div></div><h3>Casa de la playa</h3></li>
+                    <li><div></div><h3>Barrio de Grecia</h3></li>
+                    <li><div></div><h3>Desayuno ejecutivo y laptop</h3></li>
+                    <li><div></div><h3>Fresas y pan</h3></li>
+                    <li><div></div><h3>Salsas y ensaladas</h3></li>
+                </ul>
+            </div>
+            <footer>
+                <div>
+                    <div>
+                        <img src="https://static.wixstatic.com/media/90cf860d8e3548fdb4808f676ee3e05d.jpg/v1/fill/w_140,h_141,al_c,q_80,usm_0.66_1.00_0.01/90cf860d8e3548fdb4808f676ee3e05d.webp" alt="Image">
+                        <div>
+                            <h3>Sobre mí</h3>
+                            <p>Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es fácil. Haz clic en Editar Texto o doble clic aquí para agregar tu contenido y cambiar la fuente.</p>
+                            <a href="#">Leer más</a>
+                        </div>
+                    </div>
+                    <p>© 2023 Creado por Trotamundos con <a href="#">Wix.com</a></p>
+                </div>
+                <div>
+                    <form action="#">
+                        <h3>Únete a mi lista de correo</h3>
+                        <label for="email">Email *</label>
+                        <input type="text" id="email" placeholder="Ingrese su email aquí *">
+                        <button type="submit">Unirse</button>
+                    </form>
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+                <a href="#" id="arrow-up"><i class="fas fa-arrow-up"></i></a>
+            </footer>
+            <div>
+                
+            </div>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>CSS3</title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <nav>
+            
+        </nav>
+        <section>
+            <aside>
+                
+            </aside>
+            <div>
+                
+            </div>
+        </section>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
